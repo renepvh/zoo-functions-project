2c8f9db77950d602ea34080561b1296fefa191f5
Renê Alves <renepvh11@gmail.com>
2022-03-16 15:28:32 -0400

    requisito 2 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // usei o find primeiro porque precisa filtrar pelo nome do animal.
+  const nameAnimal = species.find((item) => item.name === animal);
+  // apos filtrar o nome do animal, so tinha que acessar a chave residents e percorrer por todas as age contida pra ver se passava pela condiçao.
+  return nameAnimal.residents.every((item) => item.age > age);
 }
 
 module.exports = getAnimalsOlderThan;

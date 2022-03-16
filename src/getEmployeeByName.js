fc9167a86e08d3830626c27e8e821d38b059def2
Renê Alves <renepvh11@gmail.com>
2022-03-16 16:42:05 -0400

    requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName !== undefined) {
+    const first = employees.find((item) => employeeName === item.firstName);
+    const last = employees.find((item) => employeeName === item.lastName);
+    return first || last;
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;

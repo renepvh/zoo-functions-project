701fdad3c18e4ec352b31c27e695dc86c9d227ad
Renê Alves <renepvh11@gmail.com>
2022-03-22 14:52:13 -0400

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const allAnimals = () => species.reduce((acc, { name, residents }) => {
+  acc[name] = residents.length;
+  return acc;
+}, {});
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return allAnimals();
+  }
+  const animaisRes = species.find(({ name }) => name === animal.specie).residents;
+  if (!animal.sex) {
+    return animaisRes.length;
+  }
+  return animaisRes.filter(({ sex }) => sex === animal.sex).length;
 }
 
 module.exports = countAnimals;

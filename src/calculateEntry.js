4d131a84d26e390c921e1c21fc996992651b1261
Renê Alves <renepvh11@gmail.com>
2022-03-22 12:16:07 -0400

    requisito 6 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const publicTotal = { adult: 0, senior: 0, child: 0 };
+  entrants.forEach((publiC) => {
+    if (publiC.age < 18) {
+      publicTotal.child += 1;
+    } else if (publiC.age < 50) {
+      publicTotal.adult += 1;
+    } else {
+      publicTotal.senior += 1;
+    }
+  });
+  return publicTotal;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const participants = countEntrants(entrants);
+  const entrance = [prices];
+  entrance.push(participants);
+
+  const result = entrance
+    .reduce(((ac, cur) => ac.child * cur.child + ac.adult * cur.adult + ac.senior * cur.senior));
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };

821cda05ca6900776d487e15f8b8560fd11fc79e
Renê Alves <renepvh11@gmail.com>
2022-03-22 22:54:10 -0400

    requisito 8 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(employeeId) {
+  const oldestAnimal = Object.values(species.find((specie) =>
+    specie.id === employees
+      .find(({ id }) => id === employeeId).responsibleFor[0]).residents
+    .reduce((acc, curr) => (acc.age < curr.age ? curr : acc)));
+  return oldestAnimal;
 }
 
 module.exports = getOldestFromFirstSpecies;
